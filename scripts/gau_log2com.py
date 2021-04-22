7ba26a7f82ceeaecbb23006d5f87e209aadbc915
diogom <diogom@scripps.edu>
2021-04-21 21:33:59 -0700

    log2com -o AUTO returns lowest maxforce opt_step

--- scripts/gau_log2com.py
+++ scripts/gau_log2com.py
@@ -29,9 +29,8 @@ def get_args():
                         default=0,
                         type=int)
     parser.add_argument('-o', '--opt_step',
-                        help='opt step  (default = last = 0)',
-                        default=0,
-                        type=int)
+                        help='opt step  (default = last = 0, -1 for AUTO)',
+                        default=0)
     parser.add_argument('--new_com',
                         help='new com name (default = increment template com)',
                         default='')
@@ -49,9 +48,29 @@ def get_args():
             '{0} already exists. Aborting.\n'.format(args.new_com))
         sys.exit(2)
     args.scan_step -= 1
-    args.opt_step -= 1
-
+    try:
+        args.opt_step = int(args.opt_step)
+        args.opt_step -= 1
+    except:
+        if args.opt_step != 'AUTO':
+            sys.stderr.write('--opt_step must be integer or "AUTO"\n')
+            sys.exit()
+            
     return args
+
+def find_most_converged(gaulog, scan_step):
+    """ find opt step with lowest maxforce"""
+    lowest_maxforce = float('inf')
+    index_best = None
+    index = 0
+    for byte in gaulog.bytedict['Converged?'][scan_step]:
+        (labels, values, thresh) = gaulog.read_converged(byte)
+        maxforce, avgforce, maxdisp, avgdisp = values
+        if maxforce < lowest_maxforce:
+            lowest_maxforce = maxforce
+            index_best = index
+        index += 1
+    return index_best
 
 def main():
     """
@@ -61,6 +80,8 @@ def main():
     args = get_args()
     gaulog = GaussianLog(args.log)
     gaucom = GaussianCom(args.template_com)
+    if args.opt_step == 'AUTO':
+        args.opt_step = find_most_converged(gaulog, args.scan_step)
     atoms_log_coords = gaulog.read_geometry(args.opt_step, args.scan_step)
     for no, atom in enumerate(gaucom.atoms_list):
         atom.SetVector(atoms_log_coords[no].GetVector())
